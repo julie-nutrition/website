a89cf198dc3ebd08dfa192cb9fda70f4564c4909
Justin GALLY <justin@jgcoding.com>
2026-08-30 17:12:09 +0200

    test: update homepage end-to-end coverage

--- tests/e2e/frontend.e2e.spec.ts
+++ tests/e2e/frontend.e2e.spec.ts
@@ -1,20 +1,11 @@
-import { test, expect, Page } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 test.describe('Frontend', () => {
-  let page: Page
-
-  test.beforeAll(async ({ browser }, testInfo) => {
-    const context = await browser.newContext()
-    page = await context.newPage()
-  })
-
   test('can go on homepage', async ({ page }) => {
     await page.goto('http://localhost:3000')
 
-    await expect(page).toHaveTitle(/Payload Blank Template/)
-
-    const heading = page.locator('h1').first()
-
-    await expect(heading).toHaveText('Welcome to your new project.')
+    await expect(page).toHaveTitle('Julie BAUZA - Nutritionniste')
+    await expect(page.locator('main a[href="/batchcooking"]')).toBeVisible()
+    await expect(page.locator('main a[href="/nutrition"]')).toBeVisible()
   })
 })
